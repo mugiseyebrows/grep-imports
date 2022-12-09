ea94f526a589a386e231970fed375cffafd11ef2
Doronin Stanislav <mugisbrows@gmail.com>
2022-12-09 14:34:06 +0300

    0.0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# grep-imports
+
+Greps sources for imports and prints dependencies as list of package names
+
+# Install
+
+```
+pip install grepimports
+```
+
+# Use
+
+```
+grepimports /path/to/sources > requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    packages = find_packages(),
+    name = 'grepimports',
+    version='0.0.1',
+    author="Stanislav Doronin",
+    author_email="mugisbrows@gmail.com",
+    url='https://github.com/mugiseyebrows/grep-imports',
+    description="Greps sources for imports and prints dependencies as list of package names",
+    long_description = long_description,
+    install_requires = ['importlib_metadata'],
+    entry_points={
+        'console_scripts': [
+            'grepimports = grepimports:main'
+        ]
+    },
+)
